0707c48072efbff1aa1a6d2cb0fc482569ef1889
he514434 <514434@qq.com>
2018-08-31 19:54:24 +0800

    kaoshi

---
+++ 张渠/js/move.js
@@ -0,0 +1,35 @@
+function move(ele,json,callback){
+	clearInterval(ele.timer);
+	ele.timer = setInterval(function(){
+		var onOff = true;
+		for(var attr in json){
+			var iNow = attr=="opacity" ? getStyle(ele,attr)*100 : parseInt(getStyle(ele,attr))
+
+			var speed = (json[attr]-iNow)/6;
+			speed = speed<0 ? Math.floor(speed) : Math.ceil(speed);
+
+			if(json[attr] != iNow){
+				onOff = false;
+			}
+
+			if(attr == "opacity"){
+				ele.style.opacity = (iNow+speed)/100;
+				ele.style.filter = "alpha(opacity="+ (iNow+speed) +")";
+			}else{
+				ele.style[attr] = iNow+speed + "px";
+			}
+		}
+		if(onOff){
+			clearInterval(ele.timer);
+			if(callback) callback();
+		}
+	},30)
+}
+
+function getStyle(ele,attr){
+	if(ele.currentStyle){
+		return ele.currentStyle[attr]
+	}else{
+		return getComputedStyle(ele,false)[attr];
+	}
+}
